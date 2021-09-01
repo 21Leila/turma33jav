d7246f2939e23b05b8cab93de9ccbf95271b8026
21Leila <leilafernandadasilva@gmail.com>
2021-09-01 19:13:24 -0300

    Exercicio Lista 2

---
+++ AULAS/AULAS/src/Lista2/Exercicio1Lacos.java
@@ -0,0 +1,35 @@
+package Lista2;
+
+import java.util.Scanner;
+
+public class Exercicio1Lacos {
+
+
+public static void main (String[] args) {
+
+    Scanner leia = new Scanner(System.in);
+
+    int n1, n2, n3;
+
+    System.out.println("Digite o primeiro número: ");
+    n1 = leia.nextInt();
+
+    System.out.println("Digite o segundo número: ");
+    n2 = leia.nextInt();
+
+    System.out.println("Digite o terceiro número: ");
+    n3 = leia.nextInt();
+
+    if((n1 > n2) && (n1 > n3)) {
+        System.out.println("O número maior é o primeiro! ");
+    }
+    else if((n2 > n1) && (n2 > n3)) {
+        System.out.println("O número maior é o segundo! ");
+    }
+    else if((n3 > n1) && (n3 > n2)) {
+    System.out.println("O número maior é o terceiro! ");
+    }
+
+}
+
+}
