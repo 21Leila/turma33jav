7a0924a5fc3c577e404e86d580942e8e35d22d66
21Leila <leilafernandadasilva@gmail.com>
2021-09-09 19:04:39 -0300

    Exercicio do Animal e o projeto do Banco

---
+++ AULAS/AULAS/src/Classes/Cavalo.java
@@ -0,0 +1,33 @@
+package classes;
+
+public class Cavalo extends Animal implements Treinamento{
+
+	public Cavalo(String nome, int idade) {
+		super(nome, idade);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void correr() {
+		System.out.println("Cavalo correndo....");
+	}
+	@Override
+	@Override 
+	public void emitirSom() {
+		System.out.println("Irccccc irccccc.");
+	}
+
+	@Override
+	public void rolarChao() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void emDuasPatas() {
+		System.out.println("Cavalo em duas patas....");
+
+	
+
+
+}
+}
