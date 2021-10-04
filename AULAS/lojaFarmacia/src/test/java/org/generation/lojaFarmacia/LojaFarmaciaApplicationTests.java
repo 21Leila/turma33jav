fd3d04b63bb105116aa40aea9c68a63b3e5bae11
21Leila <leilafernandadasilva@gmail.com>
2021-10-03 21:31:46 -0300

    Loja Farmácia

---
+++ AULAS/lojaFarmacia/src/test/java/org/generation/lojaFarmacia/LojaFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.lojaFarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
